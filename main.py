b05310ecaad6b280baa2f933fa1c0c2512f23407
Matthew-Linders <mlinder3@uwo.ca>
2026-03-09 21:24:57 -0400

    Correct board colours!

--- main.py
+++ main.py
@@ -12,29 +12,40 @@ def drawBoard():
     screen.blit(board, (0, 0))
 
 def drawGrid(surface):
-    WHITE = (200, 200, 200) # White for line colour
+    WHITE = (250, 250, 250) # White for line colour
+    BLACK = (0, 0, 0)
+
     WIDTH = surface.get_width()//41 #Set the size of the grid block
     HEIGHT = surface.get_height()//7
+
+    GOLD = (255, 176, 57) # Orange
+    SILVER = (220,220,220) # Grey
+    OIL = (255, 217, 158) # Pale orange
+    BONDS = (207, 253, 188) # Pale green
+    INDUST = (255, 196, 218) # Pale pink
+    GRAIN = (253, 233, 146) # Pale yellow
+    colours = [WHITE, GOLD, SILVER, OIL, BONDS, INDUST, GRAIN, WHITE]
+
     num = 0
     names = ["", "Gold", "Silver", "Oil", "Bonds", "Indust.", "Grain"]
 
     for x in range(0, surface.get_width(), WIDTH):
         for y in range(0, surface.get_height(), HEIGHT): #In theory we only need 6
             rect = pygame.Rect(x, y, WIDTH, HEIGHT)
-            pygame.draw.rect(surface, (y//HEIGHT*20, y//HEIGHT*20, y//HEIGHT*20), rect, 0)
-            pygame.draw.rect(surface, WHITE, rect, 1)
+            pygame.draw.rect(surface, colours[y//HEIGHT], rect, 0)
+            pygame.draw.rect(surface, BLACK, rect, 1)
 
             if (y==0 and x>0):
                 pygame.font.init()
                 font = pygame.Font(None, 30)
-                text = font.render((f"{num}"), False, (255, 0, 0))
+                text = font.render((f"{num}"), False, BLACK)
                 text = pygame.transform.rotate(text, -90)
                 surface.blit(text, rect)
                 num += 5
     
             if (x==0 and (y//HEIGHT)<7):
                 font2 = pygame.Font(None, 25)
-                name = font2.render((f"{names[y//HEIGHT]}"), False, (255, 0, 0))
+                name = font2.render((f"{names[y//HEIGHT]}"), False, BLACK)
                 name = pygame.transform.rotate(name, -90)
                 surface.blit(name, rect)
             
@@ -76,8 +87,7 @@ while running:
     pygame.display.flip()
 
     # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
+    # dt is delta time in seconds since last frame, used for framerate-independent physics.
     dt = clock.tick(60) / 1000
 
 pygame.quit()
